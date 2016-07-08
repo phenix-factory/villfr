9c8edded282b1ab657aee555cbef4c6d171c658a
Debondt Didier <p@henix.be>
2016-07-08 13:04:46 +0200

    Placeholder spécifique

---
+++ lang/villefr_fr.php
@@ -0,0 +1,10 @@
+<?php
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'selection_departement' => 'Séléctionnez un département',
+	'selection_ville' => 'Séléctionnez une ville',
+	'selection_region' => 'Séléctionnez une region'
+);
